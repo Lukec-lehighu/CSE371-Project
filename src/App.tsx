42972c7d165379c16a1b2177b328242fddee0a3f
Luke Crimi <lukecrimi15@gmail.com>
2025-11-10 20:59:54 -0500

    Home Button

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Receipts } from "./pages/Receipts";
 import { NotFound } from "./pages/NotFound";
 import { Requests } from "./pages/Requests";
 import { Groups } from "./pages/Groups";
+import { HomeButton } from "./components/HomeButton";
 
 function App() {
   const [pageID, setPageID] = useState(0); // ID 0: always start on sign in screen
@@ -18,12 +19,16 @@ function App() {
           <SignIn setPageID={setPageID}/>
         );
       case 1:
+        //this is kind of the home page ig
         return (
           <Groups setPageID={setPageID} setGroupName={setGroupName}/>
         );
       case 2:
         return (
-          <GroupHome groupName={currGroupName}/>
+          <>
+            <GroupHome groupName={currGroupName}/>
+            <HomeButton setPageID={setPageID}/>
+          </>
         );
       case 3:
         return (
