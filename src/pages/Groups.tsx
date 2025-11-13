c249b7c93995478a27b3366073cad07d5696de22
Luke Crimi <lukecrimi15@gmail.com>
2025-11-13 00:01:28 -0500

    WIP: trying to fix broken button

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -109,8 +109,8 @@ export const Groups = ({setPageID, setGroupName}: Props) => {
         </>
       }
 
-      <div data-bs-toggle="modal" data-bs-target="#newGroupModal">
-        <button className="btn btn-primary" style={{position: "absolute", bottom:20, right:20}}>
+      <div data-bs-toggle="modal" data-bs-target="#newGroupModal" style={{position: "absolute", bottom:20, right:20, zIndex:10}}>
+        <button className="btn btn-primary">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle m-2" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
