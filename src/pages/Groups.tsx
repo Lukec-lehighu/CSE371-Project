a979b031bada058a747c350f52c6ae7c5609524f
CodeSample15 <lukecrimi15@gmail.com>
2025-11-13 01:22:06 -0500

    Fixed broken button

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -103,14 +103,14 @@ export const Groups = ({setPageID, setGroupName}: Props) => {
               ))
             }
           </ul>
-          <div className="d-flex justify-content-center fixed-bottom m-3">
+          <div className="d-flex justify-content-center" style={{position: "fixed", bottom:20}}>
             <button className="btn btn-dark p-3" onClick={loadGroups}>Refresh</button>
           </div>
         </>
       }
 
-      <div data-bs-toggle="modal" data-bs-target="#newGroupModal" style={{position: "absolute", bottom:20, right:20, zIndex:10}}>
-        <button className="btn btn-primary">
+      <div data-bs-toggle="modal" data-bs-target="#newGroupModal">
+        <button className="btn btn-primary" style={{position: "fixed", bottom:20, right:20, zIndex:10}}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle m-2" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
